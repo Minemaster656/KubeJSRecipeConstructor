9e63ded9da193c2ba4045b08e01fb62c9bfe6221
minemaster <glebbasmanov123@gmail.com>
2024-08-11 22:56:14 +0400

    пофиксил автокомплит

--- autocomplete-manager.js
+++ autocomplete-manager.js
@@ -20,23 +20,29 @@ for (let i = 0; i < autocomplete_fields.length; i++) {
         selected_autocomplete_field.classList.add("last-selected-autocomplete");
     });
     autocomplete_fields[i].addEventListener("input", () => {
-        console.clear()
+        // console.clear()
         let val = autocomplete_fields[i].value;
+        let val_no_tag = val.startsWith("#") ? val.split("").splice(1).join("") : val
         let suggestions_raw = []
         for (let j = 0; j < autocomplete.length; j++) {
             if (val[0] === "#" & !autocomplete[j].isTag || val[0] !== "#" & autocomplete[j].isTag) continue
             //console.log(`${autocomplete[j].isTag ? '#' : ''}${autocomplete[j].namespace}:${autocomplete[j].id}`)
+            console.log(val + "   |   " + JSON.stringify(autocomplete[j]))
+            if (val.search(":") >= 0) {
+                // console.log("^^^ has namespace")
 
-            if (val.search(":") !== -1) {
-                console.log(autocomplete[j])
                 let namespace = val.split(":")[0]
+                namespace = namespace.startsWith("#") ? namespace.split("").splice(1).join("") : namespace
                 if (autocomplete[j].namespace != namespace) continue
                 if (autocomplete[j].id.startsWith(val.split(":")[1]) == false) continue //TODO: поиск в середине а не только в начале (search). сделай
             }
             else {
-                if (autocomplete[j].id.search(val) === -1 && autocomplete[j].namespace.search(val) !== -1) continue 
+                // console.log("^^^ no namespace")
+                // console.log(JSON.stringify(autocomplete[j]) + "   |   " + "id.search(" + val_no_tag + ") = " + autocomplete[j].id.includes(val_no_tag) + "   |   " + "namespace.search(" + val + ") = " + autocomplete[j].namespace.includes(val))
+                //^^^ не везде val на val_no_tag заменен и search не заменен в строках на inncludes
+                if (!autocomplete[j].id.includes(val_no_tag) && !autocomplete[j].namespace.includes(val_no_tag)) continue
             }
-            console.log(autocomplete[j])
+
             suggestions_raw.push(autocomplete[j])
 
 
@@ -48,13 +54,16 @@ for (let i = 0; i < autocomplete_fields.length; i++) {
 
 }
 function raw_autocompletes_to_html(autocomplete_suggestions_raw) {
-    
+
     let existing_autocompletes = document.querySelectorAll(".autocomplete-suggestion");
     let autocomplete_suggestions_strings = []
-    autocomplete_suggestions_strings = autocomplete_suggestions_raw.forEach(e => {
-        console.log(e.isTag ? "#" : "" + e.namespace + ":" + e.id)
-        return e.isTag ? "#" : "" + e.namespace + ":" + e.id
-    });
+    for (let i = 0; i < autocomplete_suggestions_raw.length; i++) {
+        autocomplete_suggestions_strings.push((autocomplete_suggestions_raw[i].isTag ? "#" : "") + autocomplete_suggestions_raw[i].namespace + ":" + autocomplete_suggestions_raw[i].id)
+    }
+    // autocomplete_suggestions_strings = autocomplete_suggestions_raw.forEach(e => {
+    //     console.log(e.isTag ? "#" : "" + e.namespace + ":" + e.id)
+    //     return e.isTag ? "#" : "" + e.namespace + ":" + e.id
+    // });
     console.log(autocomplete_suggestions_strings)
     let remove_existing = []
     let lazy_new = []
@@ -80,11 +89,24 @@ function raw_autocompletes_to_html(autocomplete_suggestions_raw) {
 
     for (let i = 0; i < autocomplete_suggestions_strings.length; i++) {
         if (lazy_new.includes(autocomplete_suggestions_strings[i])) continue
-        let li = document.createElement("li");
+        let li = document.createElement("div");
         li.classList.add("autocomplete-suggestion");
-        li.appendChild(document.createElement("div")).classList.add("item-icon-container").appendChild(document.createElement("img")).classList.add("item-icon");
-        li.appendChild(document.createElement("div")).classList.add("autocomplete-suggestion-text").appendChild(document.createElement("p").innerText = autocomplete_suggestions_strings[i]);
+        let iconcont = document.createElement("div");
+        iconcont.classList.add("item-icon-container");
+        let icon = document.createElement("img");
+        icon.classList.add("item-icon");
+        iconcont.appendChild(icon);
+        li.appendChild(iconcont);
+        let autocompleteSuggestionText = document.createElement("div");
+        autocompleteSuggestionText.classList.add("autocomplete-suggestion-text");
+        let ast_p = document.createElement("p");
+        ast_p.innerText = autocomplete_suggestions_strings[i];
+        autocompleteSuggestionText.appendChild(ast_p);
+        li.appendChild(autocompleteSuggestionText);
         document.querySelector(".autocomplete-suggestions").appendChild(li);
+        // li.appendChild(document.createElement("div")).classList.add("item-icon-container").appendChild(document.createElement("img")).classList.add("item-icon");
+        // li.appendChild(document.createElement("div")).classList.add("autocomplete-suggestion-text").appendChild(document.createElement("p").innerText = autocomplete_suggestions_strings[i]);
+        // document.querySelector(".autocomplete-suggestions").appendChild(li);
 
 
     }
