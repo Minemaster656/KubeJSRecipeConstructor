8ac417f804d21f4c0b5f03214edd93e537caba86
minemaster <glebbasmanov123@gmail.com>
2024-08-11 18:58:34 +0400

    2х2 крафт

--- processors/crafting.js
+++ processors/crafting.js
@@ -1,3 +1,4 @@
+console.log("CreaftingProcessor loaded")
 class CreaftingProcessor {
     exportDatapack() {
         return ""
@@ -6,3 +7,13 @@ class CreaftingProcessor {
         return ""
     }
 }
+document.getElementById("crafting-is2x2").addEventListener("change", () => {
+    console.log(document.getElementById("crafting-is2x2").checked)
+    document.getElementById("crafting-input-top-right").disabled = document.getElementById("crafting-is2x2").checked;
+    document.getElementById("crafting-input-middle-right").disabled = document.getElementById("crafting-is2x2").checked;
+    document.getElementById("crafting-input-bottom-right").disabled = document.getElementById("crafting-is2x2").checked;
+    document.getElementById("crafting-input-bottom-middle").disabled = document.getElementById("crafting-is2x2").checked;
+    document.getElementById("crafting-input-bottom-left").disabled = document.getElementById("crafting-is2x2").checked;
+    
+
+})
